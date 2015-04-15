bc4ad9b32d13a3598f9642a589e9680a0a3202af
Ingo Brückl <ib@wupperonline.de>
2015-04-15 22:57:42 +0200

    Add missing window icon to go-to-folder dialog

--- src/go-dialog.c
+++ src/go-dialog.c
@@ -21,6 +21,10 @@ static gboolean show_go_dialog( GtkWindow* parent, char * initial_path )
     GtkBuilder* builder = _gtk_builder_new_from_file( PACKAGE_UI_DIR "/godlg.ui", NULL );
     GtkDialog* dlg = GTK_DIALOG(gtk_builder_get_object( builder, "godlg" ));
 
+    GdkPixbuf *icon = gtk_icon_theme_load_icon( gtk_icon_theme_get_default(), "pcmanfm", 16, 0, NULL );
+    gtk_window_set_icon( GTK_WINDOW( dlg ), icon );
+    g_object_unref( icon );
+
     GtkEntry* path_entry = ( GtkEntry* ) ptk_path_entry_new ();
     gtk_entry_set_activates_default( path_entry, TRUE );
     gtk_entry_set_text( path_entry, initial_path );
